d66a7bc83637d3d1ee00148b79637dff1d280884
Leonardo Augusto <sleonardoaugusto@gmail.com>
2020-04-11 18:49:26 -0300

    Direcao class added

---
+++ carro/direcao.py
@@ -0,0 +1,37 @@
+import unittest
+
+
+class Direcao:
+    NORTE = 'N'
+    LESTE = 'L'
+    SUL = 'S'
+    OESTE = 'O'
+
+    def __init__(self):
+        self.direcao = self.NORTE
+
+    def girar_a_direita(self):
+        if self.direcao == self.NORTE:
+            self.direcao = self.LESTE
+        elif self.direcao == self.LESTE:
+            self.direcao = self.SUL
+        elif self.direcao == self.SUL:
+            self.direcao = self.OESTE
+        else:
+            self.direcao = self.NORTE
+
+
+class TestDirecao(unittest.TestCase):
+    def setUp(self) -> None:
+        self.d = Direcao()
+
+    def test_direcao(self):
+        self.assertEqual(self.d.direcao, self.d.NORTE)
+        self.d.girar_a_direita()
+        self.assertEqual(self.d.direcao, self.d.LESTE)
+        self.d.girar_a_direita()
+        self.assertEqual(self.d.direcao, self.d.SUL)
+        self.d.girar_a_direita()
+        self.assertEqual(self.d.direcao, self.d.OESTE)
+        self.d.girar_a_direita()
+        self.assertEqual(self.d.direcao, self.d.NORTE)
