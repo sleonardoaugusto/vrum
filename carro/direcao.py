5db243f9f73fcf05ae97af64aa0bd322aa1fd035
Leonardo Augusto <sleonardoaugusto@gmail.com>
2020-04-12 00:58:55 -0300

    Attribute name fix

--- carro/direcao.py
+++ carro/direcao.py
@@ -12,15 +12,15 @@ class Direcoes(enum.Enum):
 class Direcao:
 
     def __init__(self):
-        self._direcao = Direcoes.Norte.name
+        self.direcao = Direcoes.Norte.name
 
     def girar(self, direcao):
-        value = Direcoes[self._direcao].value
+        value = Direcoes[self.direcao].value
         if direcao == 'direita':
             value = 1 if value == 4 else value + 1
         if direcao == 'esquerda':
             value = 4 if value == 1 else value - 1
-        self._direcao = Direcoes(value).name
+        self.direcao = Direcoes(value).name
 
 
 class TestDirecao(unittest.TestCase):
@@ -28,20 +28,20 @@ class TestDirecao(unittest.TestCase):
         self.d = Direcao()
 
     def test_girar(self):
-        self.assertEqual(self.d._direcao, Direcoes.Norte.name)
+        self.assertEqual(self.d.direcao, Direcoes.Norte.name)
         self.d.girar(direcao='direita')
-        self.assertEqual(self.d._direcao, Direcoes.Leste.name)
+        self.assertEqual(self.d.direcao, Direcoes.Leste.name)
         self.d.girar(direcao='direita')
-        self.assertEqual(self.d._direcao, Direcoes.Sul.name)
+        self.assertEqual(self.d.direcao, Direcoes.Sul.name)
         self.d.girar(direcao='direita')
-        self.assertEqual(self.d._direcao, Direcoes.Oeste.name)
+        self.assertEqual(self.d.direcao, Direcoes.Oeste.name)
         self.d.girar(direcao='direita')
-        self.assertEqual(self.d._direcao, Direcoes.Norte.name)
+        self.assertEqual(self.d.direcao, Direcoes.Norte.name)
         self.d.girar(direcao='esquerda')
-        self.assertEqual(self.d._direcao, Direcoes.Oeste.name)
+        self.assertEqual(self.d.direcao, Direcoes.Oeste.name)
         self.d.girar(direcao='esquerda')
-        self.assertEqual(self.d._direcao, Direcoes.Sul.name)
+        self.assertEqual(self.d.direcao, Direcoes.Sul.name)
         self.d.girar(direcao='esquerda')
-        self.assertEqual(self.d._direcao, Direcoes.Leste.name)
+        self.assertEqual(self.d.direcao, Direcoes.Leste.name)
         self.d.girar(direcao='esquerda')
-        self.assertEqual(self.d._direcao, Direcoes.Norte.name)
+        self.assertEqual(self.d.direcao, Direcoes.Norte.name)
